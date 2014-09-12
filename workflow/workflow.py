fractaledmind/alfred-workflow

commit 5a22c1123c4b330f8ea73ef1d9e0538c3abe3e0f

Allow duplicate items in `filter`

workflow/workflow.py
@@ -1091,7 +1091,7 @@ class Workflow(object):
         fold_diacritics = self.settings.get('__workflows_diacritic_folding',
                                             fold_diacritics)
 
-        results = {}
+        results = []
 
         for i, item in enumerate(items):
             skip = False
@@ -1117,12 +1117,12 @@ class Workflow(object):
                 # use "reversed" `score` (i.e. highest becomes lowest) and
                 # `value` as sort key. This means items with the same score
                 # will be sorted in alphabetical not reverse alphabetical order
-                results[(100.0 / score, value.lower(), score)] = (item, score,
-                                                                  r)
+                results.append(((100.0 / score, value.lower(), score),
+                                (item, score, r)))
 
         # sort on keys, then discard the keys
-        keys = sorted(results.keys(), reverse=ascending)
-        results = [results.get(k) for k in keys]
+        results.sort(reverse=True)
+        results = [t[1] for t in results]
 
         if max_results and len(results) > max_results:
             results = results[:max_results]

tests/test_workflow.py
@@ -504,6 +504,12 @@ class WorkflowTests(unittest.TestCase):
         results = self.wf.filter('   ', data)
         self.assertEquals(len(results), 0)
 
+    def test_filter_identical_items(self):
+        """Filter: identical items are not discarded"""
+        data = ['bob', 'bob', 'bob']
+        results = self.wf.filter('bob', data)
+        self.assertEquals(len(results), len(data))
+
     def test_icons(self):
         """Icons"""
         import workflow

workflow/__init__.py
@@ -108,7 +108,7 @@ Or like this::
 
 """
 
-__version__ = '1.6.1'
+__version__ = '1.6.2'
 
 from .workflow import Workflow, PasswordNotFound, KeychainError
 from .workflow import (ICON_ERROR, ICON_WARNING, ICON_NOTE, ICON_INFO,

commit 1df79c8e548bc60702a5d064f79fda04aa1c22f8

fix ascending filter bug
Fix minor bug introduced in  5a22c1123c4b330f8ea73ef1d9e0538c3abe3e0f

workflow/workflow.py
@@ -1713,7 +1713,7 @@ class Workflow(object):
                                 (item, score, r)))
 
         # sort on keys, then discard the keys
-        results.sort(reverse=True)
+        results.sort(reverse=ascending)
         results = [t[1] for t in results]
 
         if max_results and len(results) > max_results: